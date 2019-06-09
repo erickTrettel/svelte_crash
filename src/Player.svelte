888a366c67fa61875c30895974cfab4b6f96e38a
Erick Trettel <ericktrettel@gmail.com>
2019-06-09 17:36:56 -0300

    Initial Commit

---
+++ src/Player.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    let dispatch = createEventDispatcher();
+
+	export let name;
+	export let points;
+	let showControls = false;
+
+	const addPoint = () => (points += 1);
+	const removePoint = () => (points -= 1);
+    const toggleControls = () => (showControls = !showControls);
+    const deletePlayer = () => dispatch("removeplayer", name);
+</script>
+
+<style>
+	h1 {
+		color: #204f6e;
+	}
+
+	h3 {
+		margin-bottom: 10px;
+	}
+</style>
+
+<div class="card">
+    <h1>
+    {name}
+    <button class="btn btn-sm" on:click={toggleControls}>
+        {#if showControls}-{:else}+{/if}
+    </button>
+    <button class="btn btn-danger btn-sm" on:click={deletePlayer}>x</button>
+    </h1>
+    <h3>Points: {points}</h3>
+
+    {#if showControls}
+        <button class="btn" on:click={addPoint}>+1</button>
+        <button class="btn btn-dark" on:click={removePoint}>-1</button>
+        <input type="number" bind:value={points}>
+    {/if}
+</div>
+
